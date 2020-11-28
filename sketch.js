b143d43696b5eac000f4217a4a42144aed48db12
Aadarsh MK <75124918+AADARSH-MADAN-KOLLAN@users.noreply.github.com>
2020-11-28 12:10:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,7 +44,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  trex = createSprite(windowWidth/2-550, 180, 20, 50);
+  trex = createSprite(windowWidth+10, windowHeigt/2, 20, 50);
   trex.addAnimation("running", trex_running);
   trex.addAnimation("collided", trex_collided);
   trex.scale = 0.5;
